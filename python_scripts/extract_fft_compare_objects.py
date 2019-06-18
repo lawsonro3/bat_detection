46e24695179e2c1de46bb23948c0fcad1a119c97
Isabelle Cunitz <icunitz@me.com>
2019-06-18 13:32:55 -0600

    Created script to compare bats and non-bat objects

---
+++ python_scripts/extract_fft_compare_objects.py
@@ -0,0 +1,210 @@
+# Import useful libraries
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits import mplot3d
+import os # Not currently using - figure out how to effectively use os.path.join
+
+plt.close() # Close any previous matplotlib.pyplot windows
+
+# Set up read paths
+file1 = input('Video 1 Name: ') # Try 2016-07-30_014634
+frame1 = input('Video 1 Frame Number: ')
+file2 = input('Video 2 Name: ') # Try something else
+frame2 = input('Video 2 Frame Number: ')
+frameextension = '.jpg'
+readpath1 = '/Users/icunitz/Desktop/bat_detection/frames/' + file1 + '/frame' + frame1 + frameextension # Customize this based on directories in computer
+readpath2 = '/Users/icunitz/Desktop/bat_detection/frames/' + file2 + '/frame' + frame2 + frameextension
+
+# Set window names
+frameTitle1 = 'Video ' + file1 + ' Frame ' + frame1
+window1Name = frameTitle1
+frameTitle2 = 'Video ' + file2 + ' Frame ' + frame2
+window2Name = frameTitle2
+
+n = 20 # Half of length of square sides
+
+ref_location1 = [] # Empty list to hold click locations on Frame 1
+
+# Read original frame 1 and create grayscale copy
+img1 = cv2.imread(readpath1)
+clone1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+# Click event 1
+def click_event1(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ref_location1.append((x, y))
+
+# Set up original frame 1 window and callback function
+cv2.namedWindow(window1Name)
+cv2.setMouseCallback(window1Name, click_event1)
+
+print('\nClick in the middle of the bat and press any key to progress. The region of interest coordinates will be saved as your last click.\n')
+
+# Show original frame 1
+cv2.imshow(window1Name, img1)
+cv2.waitKey(0) & 0xFF
+
+# Coordinates of last clicked region
+roi1_x = ref_location1[-1][0]
+roi1_y = ref_location1[-1][1]
+
+print ('Location of Interest, Frame %s: (%s, %s)' % (frame1, roi1_x, roi1_y))
+
+print('\nPress any keys to progress.\n')
+
+# Convert original frame 1 to grayscale to show later
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+# Draw square
+cv2.rectangle(img1, (roi1_x - n, roi1_y - n), (roi1_x + n, roi1_y + n), (0, 0, 0), 2)
+
+# Show grayscale frame 1 with square
+cv2.imshow(window1Name, img1)
+cv2.waitKey(0) & 0xFF
+
+# Crop frame 1 around last clicked location
+roi1 = clone1[(roi1_y - n):(roi1_y + n), (roi1_x - n):(roi1_x + n)]
+
+ref_location2 = [] # Empty list to hold click locations on Frame 2
+
+# Read original frame 2 and create grayscale copy
+img2 = cv2.imread(readpath2)
+clone2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+cv2.namedWindow(window2Name)
+
+# Click event 2 - fix later
+def click_event2(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ref_location2.append((x, y))
+
+# Set up original image window and callback function
+cv2.setMouseCallback(window2Name, click_event2)
+
+print('\nClick in the middle of the bat and press any key to progress. The region of interest coordinates will be saved as your last click.\n')
+
+# Show original image
+cv2.imshow(window2Name, img2)
+cv2.waitKey(0) & 0xFF
+
+# Coordinates of last clicked region
+roi2_x = ref_location2[-1][0]
+roi2_y = ref_location2[-1][1]
+
+print ('Location of Interest, Frame %s: (%s, %s)' % (frame2, roi2_x, roi2_y))
+
+print('\nPress any keys to progress.\n')
+
+# Convert original frame 2 to grayscale to show later
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# Draw square around region of interest in frame 2
+cv2.rectangle(img2, (roi2_x - n, roi2_y - n), (roi2_x + n, roi2_y + n), (0, 0, 0), 2)
+
+# Show grayscale frame 2 with square
+cv2.imshow(window2Name, img2)
+cv2.waitKey(0) & 0xFF
+
+# Crop frame 2 around same location
+roi2 = clone2[(roi2_y - n):(roi2_y + n), (roi2_x - n):(roi2_x + n)]
+
+cv2.destroyAllWindows()
+
+# Define FFT function
+def takedft(img_name):
+        dft = cv2.dft(np.float32(img_name), flags = cv2.DFT_COMPLEX_OUTPUT)
+        dft_shift = np.fft.fftshift(dft)
+        magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+        return dft, dft_shift, magnitude_spectrum
+
+# Take FFT and extract magnitude spectrum of cropped images
+takedft_roi1 = takedft(roi1)
+mag_spect_roi1 = takedft_roi1[2]
+takedft_roi2 = takedft(roi2)
+mag_spect_roi2 = takedft_roi2[2]
+
+## Show results
+
+titlefontsize = 12
+subtitlefontsize = 10
+
+# Set up grayscale normalization condition
+Normalization = False
+
+if Normalization:
+        norm = mpl.colors.Normalize(vmin = 0, vmax = 255)
+else:
+        norm = None
+
+plt.figure(1, figsize=(12, 6))
+plt.suptitle('%s vs. %s Comparison' % (file1, file2), fontsize = titlefontsize)
+
+plt.subplot(2, 4, 1)
+plt.cla()
+plt.imshow(img1, cmap='gray', norm=norm)
+plt.title(frameTitle1, fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 4, 2)
+plt.cla()
+plt.imshow(roi1, cmap='gray', norm=norm)
+plt.title('ROI', fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 4, 3)
+plt.cla()
+plt.imshow(mag_spect_roi1, cmap='gray', norm=norm)
+plt.title('FFT of ROI', fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+ax1 = plt.subplot(2, 4, 4, projection='3d')
+plt.cla()
+X1, Y1 = np.meshgrid(range(2*n), range(2*n))
+Z1 = mag_spect_roi1
+mplot3d.Axes3D.plot_surface(ax1, X1, Y1, Z1, cmap='gray', norm=norm)
+plt.title('FFT of ROI, 3D', fontsize = subtitlefontsize)
+mplot3d.Axes3D.set_zlim3d(ax1, bottom=0.0, top=200.0)
+mplot3d.Axes3D.set_zticks(ax1, [])
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 4, 5)
+plt.cla()
+plt.imshow(img2, cmap='gray', norm=norm)
+plt.title(frameTitle2, fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 4, 6)
+plt.cla()
+plt.imshow(roi2, cmap='gray', norm=norm)
+plt.title('ROI', fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 4, 7)
+plt.cla()
+plt.imshow(mag_spect_roi2, cmap='gray', norm=norm)
+plt.title('FFT of ROI', fontsize = subtitlefontsize)
+plt.xticks([])
+plt.yticks([])
+
+ax2 = plt.subplot(2, 4, 8, projection='3d')
+plt.cla()
+X2, Y2 = np.meshgrid(range(2*n), range(2*n))
+Z2 = mag_spect_roi2
+mplot3d.Axes3D.plot_surface(ax2, X2, Y2, Z2, cmap='gray', norm=norm)
+plt.title('FFT of ROI, 3D', fontsize = subtitlefontsize)
+mplot3d.Axes3D.set_zlim3d(ax2, bottom=0.0, top=200.0)
+mplot3d.Axes3D.set_zticks(ax2, [])
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
+
+print ("Done")
