ddf5984bf49d3e21e9a7348c59dc1dd1f7fa590b
Isabelle Cunitz <icunitz@me.com>
2019-06-28 11:37:09 -0600

    Did practice work to fix same_location script

--- python_scripts/rotate_image_practice.py
+++ python_scripts/rotate_image_practice.py
@@ -12,14 +12,31 @@ img = cv2.imread('/Users/icunitz/Desktop/bat_detection/frames/2016-07-30_014634/
 
 rows, cols = img.shape[:2]
 
-a_deg = 90
-a_rad = a_deg * (np.pi / 180)
-r = int(rows*np.cos(a_rad) + cols*np.sin(a_rad))
-c = int(cols*np.cos(a_rad) + rows*np.sin(a_rad))
+a_deg = 110
 
-M = cv2.getRotationMatrix2D((cols/2, rows/2), a_deg, 1)
+a_rad = a_deg * (np.pi / 180)
+if a_deg <= 90:
+        r = int(rows*np.cos(a_rad) + cols*np.sin(a_rad))
+        c = int(cols*np.cos(a_rad) + rows*np.sin(a_rad))
+        print ('Positive')
+else:
+        r = int(cols*np.cos(a_rad - np.pi/2) + rows*np.sin(a_rad - np.pi/2))
+        c = int(rows*np.cos(a_rad - np.pi/2) + cols*np.sin(a_rad - np.pi/2))
+        print ('Negative')
+M = cv2.getRotationMatrix2D((cols//2, rows//2), a_deg, 1.0)
 M[0,2] += (c - cols) / 2
 M[1,2] += (r - rows) / 2
+
+# rows, cols = img.shape[:2]
+# 
+# a_deg = 90
+# a_rad = a_deg * (np.pi / 180)
+# r = int(rows*np.cos(a_rad) + cols*np.sin(a_rad))
+# c = int(cols*np.cos(a_rad) + rows*np.sin(a_rad))
+# 
+# M = cv2.getRotationMatrix2D((cols/2, rows/2), a_deg, 1)
+# M[0,2] += (c - cols) / 2
+# M[1,2] += (r - rows) / 2
 dst = cv2.warpAffine(img, M, (c, r))
 
 cv2.imshow('Rotated', dst)
